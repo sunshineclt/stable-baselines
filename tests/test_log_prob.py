f24ce19317eb62e2d13145f532a5201ee61fc29e
SVJayanthi <sjayanthi8@gatech.edu>
2020-12-15 10:56:30 -0500

    Log prob test on different ActorCriticRL algos

--- tests/test_log_prob.py
+++ tests/test_log_prob.py
@@ -1,81 +1,30 @@
-import tensorflow as tf
+import pytest
 import numpy as np
 from gym.spaces.box import Box
 
-from stable_baselines.common.math_util import discount_with_boundaries, scale_action, unscale_action
+from stable_baselines import A2C, PPO1, PPO2, TRPO
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.identity_env import IdentityEnvBox
 
 
-def test_discount_with_boundaries():
-    """
-    test the discount_with_boundaries function
-    """
-    gamma = 0.9
-    rewards = np.array([1.0, 2.0, 3.0, 4.0], 'float32')
-    episode_starts = [1.0, 0.0, 0.0, 1.0]
-    discounted_rewards = discount_with_boundaries(rewards, episode_starts, gamma)
-    assert np.allclose(discounted_rewards, [1 + gamma * 2 + gamma ** 2 * 3, 2 + gamma * 3, 3, 4])
-    return
+class Helper:
+    @staticmethod
+    def proba_vals(obs, state, mask):
+        return np.array([-0.4]), np.array([[0.1]])
 
 
-def test_scaling_action():
-    """
-    test scaling of scalar, 1d and 2d vectors of finite non-NaN real numbers to and from tanh co-domain (per component)
-    """
-    test_ranges = [(-1, 1), (-10, 10), (-10, 5), (-10, 0), (-10, -5), (0, 10), (5, 10)]
-
-    # scalars
-    for (range_low, range_high) in test_ranges:
-        check_scaled_actions_from_range(range_low, range_high, scalar=True)
-
-    # 1d vectors: wrapped scalars
-    for test_range in test_ranges:
-        check_scaled_actions_from_range(*test_range)
-
-    # 2d vectors: all combinations of ranges above
-    for (r1_low, r1_high) in test_ranges:
-        for (r2_low, r2_high) in test_ranges:
-            check_scaled_actions_from_range(np.array([r1_low, r2_low], dtype=np.float),
-                                            np.array([r1_high, r2_high], dtype=np.float))
+@pytest.fixture
+def helpers():
+    return Helper
 
 
-def check_scaled_actions_from_range(low, high, scalar=False):
-    """
-    helper method which creates dummy action space spanning between respective components of low and high
-    and then checks scaling to and from tanh co-domain for low, middle and high value from  that action space
-    :param low: (np.ndarray), (int) or (float)
-    :param high: (np.ndarray), (int) or (float)
-    :param scalar: (bool) Whether consider scalar range or wrap it into 1d vector
-    """
+@pytest.mark.parametrize("model_class", [A2C, PPO1, PPO2, TRPO])
+def test_log_prob_calcuation(model_class, helpers):
+    env = DummyVecEnv([lambda: IdentityEnvBox()])
+    model = model_class('MlpPolicy', env)
 
-    if scalar and (isinstance(low, float) or isinstance(low, int)):
-        ones = 1.
-        action_space = Box(low, high, shape=(1,))
-    else:
-        low = np.atleast_1d(low)
-        high = np.atleast_1d(high)
-        ones = np.ones_like(low)
-        action_space = Box(low, high)
+    model.proba_step = helpers.proba_vals
 
-    mid = 0.5 * (low + high)
-
-    expected_mapping = [(low, -ones), (mid, 0. * ones), (high, ones)]
-
-    for (not_scaled, scaled) in expected_mapping:
-        assert np.allclose(scale_action(action_space, not_scaled), scaled)
-        assert np.allclose(unscale_action(action_space, scaled), not_scaled)
-
-
-def test_batch_shape_invariant_to_scaling():
-    """
-    test that scaling deals well with batches as tensors and numpy matrices in terms of shape
-    """
-    action_space = Box(np.array([-10., -5., -1.]), np.array([10., 3., 2.]))
-
-    tensor = tf.constant(1., shape=[2, 3])
-    matrix = np.ones((2, 3))
-
-    assert scale_action(action_space, tensor).shape == (2, 3)
-    assert scale_action(action_space, matrix).shape == (2, 3)
-
-    assert unscale_action(action_space, tensor).shape == (2, 3)
-    assert unscale_action(action_space, matrix).shape == (2, 3)
+    logprob = model.action_probability(np.array([[0.5],[0.5]]), 1, [False], 0.2, True)
+    assert np.all(logprob == np.array([-16.616353440210627])), \
+        "Calculation failed for {}".format(model_class)
