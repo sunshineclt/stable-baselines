6e6cdad8697679a2a4fbfd2c10ab379a959c5299
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-12-23 15:38:48 +0100

    Minor edits + update changelog

--- tests/test_log_prob.py
+++ tests/test_log_prob.py
@@ -1,30 +1,22 @@
 import pytest
 import numpy as np
-from gym.spaces.box import Box
 
-from stable_baselines import A2C, PPO1, PPO2, TRPO
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C, ACKTR, PPO1, PPO2, TRPO
 from stable_baselines.common.identity_env import IdentityEnvBox
 
 
 class Helper:
     @staticmethod
     def proba_vals(obs, state, mask):
+        # Return fixed mean, std
         return np.array([-0.4]), np.array([[0.1]])
 
 
-@pytest.fixture
-def helpers():
-    return Helper
-
-
-@pytest.mark.parametrize("model_class", [A2C, PPO1, PPO2, TRPO])
-def test_log_prob_calcuation(model_class, helpers):
-    env = DummyVecEnv([lambda: IdentityEnvBox()])
-    model = model_class('MlpPolicy', env)
-
-    model.proba_step = helpers.proba_vals
-
-    logprob = model.action_probability(np.array([[0.5],[0.5]]), 1, [False], 0.2, True)
-    assert np.all(np.abs(logprob - np.array([-16.616353440210627]))<1e-7), \
-        "Calculation failed for {}".format(model_class)
+@pytest.mark.parametrize("model_class", [A2C, ACKTR, PPO1, PPO2, TRPO])
+def test_log_prob_calcuation(model_class):
+    model = model_class("MlpPolicy", IdentityEnvBox())
+    # Fixed mean/std
+    model.proba_step = Helper.proba_vals
+    # Check that the log probability is the one expected for the given mean/std
+    logprob = model.action_probability(observation=np.array([[0.5], [0.5]]), actions=0.2, logp=True)
+    assert np.allclose(logprob, np.array([-16.616353440210627])), "Calculation failed for {}".format(model_class)
